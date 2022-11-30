273af9e71336954e0d4c98f53a51d3f12e713ea9
JooBoa <joocong2265@gmail.com>
2022-11-30 21:13:42 +0900

    Ch06. MVC 프레임워크 만들기 - 02.프런트 컨트롤러 패턴 개념 / 03. 프런트 컨트롤러 패턴 실습

---
+++ src/main/java/org/example/WebApplicationServer.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import org.slf4j.Logger;
+
+public class WebApplicationServer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapp/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080); //8080포트 설정
+
+        tomcat.addWebapp("/",new File(webappDirLocation).getAbsolutePath()); //webapp/경로를 바라보도록
+        log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
---
+++ src/main/java/org/example/mvc/controller/HomeController.java
@@ -0,0 +1,10 @@
+package org.example.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeController implements Controller{
+    public String handleRequest(HttpServletRequest request , HttpServletResponse response) throws Exception{
+        return "home.jsp";
+    }
+}
